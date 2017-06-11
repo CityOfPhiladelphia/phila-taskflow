4f0cee1514bbe8c89cef6efb37dfaaffbe3b1eb0
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-06-11 16:43:47 -0400

    Add S3 SFTP sync

--- phila_taskflow/tasks/__init__.py
+++ phila_taskflow/tasks/__init__.py
@@ -1,3 +1,5 @@
+from .s3_sftp_sync import s3_sftp_sync
 
 tasks = [
+    s3_sftp_sync
 ]
---
+++ phila_taskflow/tasks/s3_sftp_sync.py
@@ -0,0 +1,17 @@
+import os
+
+from taskflow.tasks.bash_task import BashTask
+
+class S3SFTPSync(BashTask):
+    def get_command(self):
+        return 'source <(eastern_state load_environment "$EASTERN_STATE_BUCKET" "$EASTERN_STATE_NAME" "$EASTERN_STATE_ENV") && s3_sftp_sync'
+
+s3_sftp_sync = S3SFTPSync(
+    name='s3_sftp_sync',
+    active=True,
+    schedule='0 * * * *',
+    push_destination='aws_batch',
+    timeout=7200,
+    params={
+        'job_definition': os.getenv('S3_SFTP_SYNC_JOB_DEFINITION')
+    })
